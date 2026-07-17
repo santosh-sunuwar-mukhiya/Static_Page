52253c6ada28408f3f5c02d08b509f6e6ee473eb
Santosh Sunuwar Mukhiya <santoshsunuwarmukhiya@gmail.com>
2026-07-17 11:14:41 +0545

    simple react static page design

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+    return (
+        <header>
+            <nav>
+                <img src="/src/assets/react-logo-half.png" alt="React logo" />
+                <span>ReactFacts</span>
+            </nav>
+        </header>
+    )
+}
