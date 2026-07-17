ffca9ad7417cf49185676da954fd05c82cdca3ae
Santosh Sunuwar Mukhiya <santoshsunuwarmukhiya@gmail.com>
2026-07-17 11:16:31 +0545

    final styling of the page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ export default function Navbar() {
     return (
         <header>
             <nav>
-                <img src="/src/assets/react-logo-half.png" alt="React logo" />
+                <img src="/src/assets/react-logo.png" alt="React logo" />
                 <span>ReactFacts</span>
             </nav>
         </header>
